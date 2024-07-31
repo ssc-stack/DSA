a353a8483f1a64414ed264a819bfecac3130d385
Shashank Chauhan <shashanktech2@gmail.com>
2024-07-31 12:46:35 +0530

    Printing Linked List

---
+++ dsa/src/main/java/linkedlist/ListNode.java
@@ -0,0 +1,7 @@
+package linkedlist;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
---
+++ dsa/src/main/java/linkedlist/PrintLinkedList.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+public class PrintLinkedList {
+    public void solve(ListNode A) {
+        StringBuilder sb=new StringBuilder();
+        while(A!=null) {
+            sb.append(A.val);
+            sb.append(" ");
+            A=A.next;
+        }
+        System.out.println(sb.toString());
+    }
+}
