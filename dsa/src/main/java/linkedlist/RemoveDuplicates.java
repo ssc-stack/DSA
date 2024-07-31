32672b3b297ab336de871453a525cdaefcd47f1b
Shashank Chauhan <shashanktech2@gmail.com>
2024-07-31 21:46:47 +0530

    Removed Duplicates from Sorted Linked List

---
+++ dsa/src/main/java/linkedlist/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+/*
+Problem Description
+
+Given a sorted linked list, delete all duplicates such that each element appears only once.
+
+
+
+Problem Constraints
+
+0 <= length of linked list <= 106
+ */
+
+package linkedlist;
+
+public class RemoveDuplicates {
+    public ListNode deleteDuplicates(ListNode A) {
+        ListNode itr=A;
+        while(itr!=null && itr.next!=null) {
+            while(itr.next!=null && itr.val==itr.next.val) {
+                itr.next=itr.next.next;
+            }
+            itr=itr.next;
+        }
+
+        return A;
+    }
+}
