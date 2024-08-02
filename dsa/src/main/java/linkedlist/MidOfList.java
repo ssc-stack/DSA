5dd5bebba26b1519a0de7c1c72332a9e79bddb42
Shashank Chauhan <shashanktech2@gmail.com>
2024-08-02 12:27:14 +0530

    Length of Longest Palindromic List

---
+++ dsa/src/main/java/linkedlist/MidOfList.java
@@ -0,0 +1,26 @@
+/*
+public int solve(ListNode A) {
+        ListNode slow=A,fast=A;
+
+        while(fast!=null && fast.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        return slow.val;
+    }
+ */
+package linkedlist;
+
+public class MidOfList {
+    public int solve(ListNode A) {
+        ListNode slow=A,fast=A;
+
+        while(fast!=null && fast.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        return slow.val;
+    }
+}
