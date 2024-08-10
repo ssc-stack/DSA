abf492fe33e1fb24eeead606c275f9e8bcd67ea7
Shashank Chauhan <shashanktech2@gmail.com>
2024-08-10 23:21:10 +0530

    Next Smaller Element on Left

---
+++ dsa/src/main/java/stack/NextSmallerElement.java
@@ -0,0 +1,41 @@
+/*
+Problem Description
+
+Given an array A, find the nearest smaller element G[i] for every element A[i] in the array such that the element has an index smaller than i.
+
+More formally,
+
+G[i] for an element A[i] = an element A[j] such that
+
+j is maximum possible AND
+
+j < i AND
+
+A[j] < A[i]
+
+Elements for which no smaller element exist, consider the next smaller element as -1.
+ */
+package stack;
+
+import java.util.*;
+
+public class NextSmallerElement {
+    public ArrayList<Integer> prevSmaller(ArrayList<Integer> A) {
+        Deque<Integer> stack=new ArrayDeque<>();
+        ArrayList<Integer> res=new ArrayList<>();
+        res.add(-1);
+        stack.push(A.get(0));
+
+        for(int i=1;i<A.size();++i) {
+            while(!stack.isEmpty() && stack.peek()>=A.get(i)) {
+                stack.pop();
+            }
+            if(stack.size()==0) res.add(-1);
+            else res.add(stack.peek());
+            stack.push(A.get(i));
+        }
+
+        return res;
+
+    }
+}
