1a856ea0a350a374ec52ea982c0cc3c6f8507fae
Shashank Chauhan <shashanktech2@gmail.com>
2024-08-15 22:24:38 +0530

    preorder traversal using stack

--- dsa/src/main/java/tree/PreOrderTraversal.java
+++ dsa/src/main/java/tree/PreOrderTraversal.java
@@ -1,8 +1,25 @@
 package tree;
 
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 
 public class PreOrderTraversal {
+
+    public void preOrderUsingStack(TreeNode root,List<Integer> list) {
+        Deque<TreeNode> stack=new ArrayDeque<>();
+        if(root!=null) stack.push(root);
+
+        while(!stack.isEmpty()) {
+            TreeNode node=stack.pop();
+            list.add(node.val);
+            if(node.right!=null)
+            stack.push(node.right);
+            if(node.left!=null)
+            stack.push(node.left);
+        }
+    }
+
     public void preOrder(TreeNode root,List<Integer> list) {
         if(root==null) return;
         list.add(root.val);
