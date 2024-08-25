9504fbd70cc7749a352f24c43678dd645b5892b2
Shashank Chauhan <shashanktech2@gmail.com>
2024-08-25 08:47:13 +0530

    Single Element in Sorted Array

--- dsa/src/main/java/searching/SingleElementInSortedArray.java
+++ dsa/src/main/java/searching/SingleElementInSortedArray.java
@@ -11,26 +11,20 @@ NOTE: Users are expected to solve this in O(log(N)) time.
 package searching;
 
 public class SingleElementInSortedArray {
-    private static int getFirstIndex(int[] A,int index) {
-        if(index!=0 && A[index]==A[index-1]) {
-            return index-1;
-        } else {
-            return index;
-        }
-    }
+    public int singleNonDuplicate(int[] nums) {
+        int n=nums.length;
+        if(nums.length==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
 
-    private static int getSingleElement(int[] A) {
-        int low=0,high=A.length-1;
+        int low=1,high=n-2;
 
         while(low<=high) {
             int mid=low+(high-low)/2;
 
-            if((mid==0 || A[mid]!=A[mid-1]) && (mid==A.length-1 || A[mid]!=A[mid+1])) {
-                return A[mid];
-            }
-
-            int firstIndex=getFirstIndex(A,mid);
-            if(firstIndex%2==0) {
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) {
+                return nums[mid];
+            } else if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1])) {
                 low=mid+1;
             } else {
                 high=mid-1;
@@ -38,9 +32,5 @@ public class SingleElementInSortedArray {
         }
 
         return -1;
-    }
-
-    public int solve(int[] A) {
-        return getSingleElement(A);
     }
 }
